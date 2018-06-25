52b1e245f6649232c29a53b55d719ac50efbdea5
Ben Hawley <benhawley0@gmail.com>
2018-06-25 15:50:49 -0400

    message

--- client/index.js
+++ client/index.js
@@ -1,31 +1,123 @@
 // Initialize sockets
 
-// const   
-let initiator = true;
+let initiator = false;
+let config = null;
 console.log('Running index.js')
 
 const socket = io.connect('http://localhost:3000');
 const input = document.getElementById('input');
 
 input.addEventListener('click', () => {
-    socket.emit('message', 'Hi there!');
+    // socket.emit('message', 'Hi there!');
+    dataChannel.send('Wait....whats up bitch????')
 })
 
 socket.on('message', (input) => {
-    console.log(input);
+    // console.log('Message received:', input);
+    signalingMessageCallback(input);
+})
+
+socket.on('created', () => {
+    initiator = true;
+    // console.log('I created the room')
 })
 
 socket.on('joined', () => {
-    initiator = false;
-    console.log('Im the second to join')
+    // initiator = false;
+    // console.log('Im the second to join')
     createPeerConnection();
 })
 
 socket.on('ready', () => {
-    console.log('Now I know the server is ready')
+    // console.log('Now I know the server is ready')
     createPeerConnection();
 })
 
-function createPeerConnection() {
-    console.log('Creating a peer connection');
+
+let pc;
+let dataChannel;
+
+createPeerConnection = () => {
+    console.log('Creating peer connection!');
+    pc = new RTCPeerConnection(config);
+    pc.onicecandidate = (event) => {
+        console.log('onicecandidate triggered!');
+        if (event.candidate) {
+            console.log('onicecandidate event is:', event.candidate);
+            sendMessage({
+                type: 'candidate',
+                label: event.candidate.sdpMLineIndex,
+                id: event.candidate.sdpMid,
+                candidate: event.candidate.candidate
+            })
+        }
+        else console.log('Out of candidates');
+    }
+    if (initiator) {
+        // Create the data channel, label it messages
+        dataChannel = pc.createDataChannel('messages');
+        // Set up handlers for new data channel
+        onDataChannelCreated(dataChannel)
+
+        // Create offer, then pass to onLocalDescription
+        // (which sets as local description and sends it)
+        pc.createOffer(onLocalDescription, logError);
+    }
+    else {
+        // Create an ondatachannel handler to respond
+        // when the data channel from the other client arrives
+        pc.ondatachannel = (event) => {
+            dataChannel = event.channel
+            onDataChannelCreated(dataChannel);
+        }
+    }
+}
+
+signalingMessageCallback = (message) => {
+    if (message.type === 'candidate') {
+        // console.log('Received icecandidate:', message.candidate);
+        pc.addIceCandidate(new RTCIceCandidate({candidate: message.candidate}));
+        console.log('After ice, RD sdp is:', pc.remoteDescription.sdp)
+    }
+    else if (message.type === 'answer') {
+        console.log('Received answer:', message);
+        pc.setRemoteDescription(new RTCSessionDescription(message), () => {}, logError);
+        console.log('After answer, RD sdp is:', pc.remoteDescription.sdp)
+    }
+    else if (message.type === 'offer') {
+        console.log('Received offer:', message);
+        pc.setRemoteDescription(new RTCSessionDescription(message), () => {}, logError)
+        console.log('After offer, RD sdp is:', pc.remoteDescription.sdp);
+        pc.createAnswer(onLocalDescription, logError);
+    }
+}
+
+onDataChannelCreated = channel => {
+    console.log('Called onDataChannelCreated');
+    channel.onopen = () => {
+        console.log('Channel opened');
+    }
+
+    channel.onclose = () => {
+        console.log('Channel closed');
+    }
+
+    channel.onmessage = message => {
+        console.log('Received this message through WebRTC:', message.data);
+    }
+}
+
+onLocalDescription = desc => {
+    pc.setLocalDescription(desc, () => {
+        console.log('Sending local description:', pc.localDescription);
+        sendMessage(pc.localDescription)
+    }, logError);
+}
+
+sendMessage = message => {
+    socket.emit('message', message);
+}
+
+logError = err => {
+    console.log('Error:', err);
 }
--- server/server.js
+++ server/server.js
@@ -6,8 +6,6 @@ const app = express();
 const server = http.Server(app); 
 const io = socketIO(server);
 
-// app.use(express.static(path.join(__dirname, './../client')));
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './../client/index.html'));
 });
@@ -21,27 +19,26 @@ io.on('connection', function(socket) {
     socket.join('myroom2');
     var clientsInRoom = io.sockets.adapter.rooms['myroom2'];
     var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+
+    if (numClients === 1) {
+        socket.emit('created');
+    }
    
     if (numClients === 2) {
+        console.log('initiating the CPC');
         socket.emit('joined');
         io.sockets.in('myroom2').emit('ready');
         socket.broadcast.emit('ready');
     }
-    console.log('Now connected to new socket');
+    // console.log('Now connected to new socket');
 
     socket.on('message', (messageReceived) => {
-        console.log(messageReceived);
-        socket.emit('message', 'you sent a message');
+        // console.log(messageReceived);
+        // socket.emit('message', 'you sent a message');
         socket.broadcast.emit('message', messageReceived);
     })
 
-    // socket.on('disconnect', function(socket) { 
-    //     clientsInRoom = io.sockets.adapter.rooms['myroom2'];
-    //     numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0; 
-    //     console.log('nunClients is', numClients);
-    // });    
-
-    console.log('nunClients is', numClients);
+    console.log('numClients is', numClients);
 })
 
 server.listen(3000, () => console.log('Listening on port 3000'));
