10fb059f75e4f42a3d6e8375948043a17615e943
Shahrukh <jalilshahrukh@gmail.com>
2018-07-05 12:48:40 -0400

    Images coming from AWS.

--- server/server.js
+++ server/server.js
@@ -5,33 +5,16 @@ const path = require('path');
 const app = express(); 
 const server = http.Server(app); 
 const io = socketIO(server);
-const AWS = require('aws-sdk'); 
+const aws = require('./awsController.js'); 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './../client/index.html'));
 });
 
+app.get('/images', aws.getImages); 
+
 app.get('/myJS', (req, res) => {
     res.sendFile(path.join(__dirname, './../client/index.js'))
-}); 
-
-// Request to AWS 
-AWS.config.update( 
-  { 
-    accessKeyId: 'AKIAJRYMO3W4PXU2NN3Q', 
-    secretAccessKey: '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'
-  }
-); 
-var params = {Bucket: 'boxchatimages'};
-var s3 = new AWS.S3(); 
-s3.listObjects(params, function(err, data) { 
-  var bucketContents = data.Contents; 
-  for (var i = 0; i < bucketContents.length; i++) {
-    var urlParams = {Bucket: 'boxchatimages', Key: bucketContents[i].Key}; 
-    s3.getSignedUrl('getObject', urlParams, function(err, url) { 
-      console.log('Url of the image is', url); 
-    }); 
-  }
 }); 
 
 io.on('connection', function(socket) {
